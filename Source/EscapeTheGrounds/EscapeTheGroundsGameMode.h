16bb8360a0010f7aad8aab4480d2477dbe820a3f
DESKTOP-2NSGSF6\gamer <gamerz1497@live.com>
2017-09-15 14:22:53 +0200

    Enabled Git LFS

---
+++ Source/EscapeTheGrounds/EscapeTheGroundsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeTheGroundsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEscapeTheGroundsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEscapeTheGroundsGameMode();
+};
+
+
+
